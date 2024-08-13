9acbe9fa9d4332e67fe6b9676cbf636d49ebc728
Jason <768747811@qq.com>
2024-08-13 23:04:07 +0800

    test mapping

---
+++ smooks-example-test/src/java/Test.java
@@ -0,0 +1,9 @@
+
+
+public class Test {
+    public static void main(String[] args) throws Exception {
+        SmooksTransform smooksTransform = new SmooksTransform();
+        String result = smooksTransform.runSmooksTransform();
+        System.out.println(result);
+    }
+}
